f415b012691f85978c6720542858756971bfd023
ppolle <peter.m.polle@gmail.com>
2018-03-18 19:28:51 +0300

    Added date functionality

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
 	public showDetails:boolean;
 	
-	constructor(public id:number, public quote:string, public name:string, public author:string){
+	constructor(public id:number, public quote:string, public name:string, public author:string, public dateCreated:Date){
 		this.showDetails=false
 		
 	}
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import {Quote} from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes = [
-  new Quote(1,'Moral indignation is jealousy with a halo','Peter Polle','H. G. Wells'),
-  new Quote(2,'Glory is fleeting, but obscurity is forever','George Raburu','Napoleon Bonaparte'),
-  new Quote(3,'The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts','Willis Kimani','Bertrand Russell'),
-  new Quote(4,'Victory goes to the player who makes the next-to-last mistake','Mahdy Twalib','Chessmaster Savielly Grigorievitch Tartakower'),
+  new Quote(1,'Moral indignation is jealousy with a halo','Peter Polle','H. G. Wells',new Date(2018,3,14)),
+  new Quote(2,'Glory is fleeting, but obscurity is forever','George Raburu','Napoleon Bonaparte',new Date(2018,3,14)),
+  new Quote(3,'The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts','Willis Kimani','Bertrand Russell',new Date(2018,3,14)),
+  new Quote(4,'Victory goes to the player who makes the next-to-last mistake','Mahdy Twalib','Chessmaster Savielly Grigorievitch Tartakower',new Date(2018,3,14)),
   ]
 
 toogleDetails(index){
@@ -23,7 +23,7 @@ deleteQuote(isDelete,index){
       if (isDelete){
 
         let toDelete=confirm(`Are you sure you want to delete the quote below?
-          
+
 ${this.quotes[index].quote}`)
 
 
